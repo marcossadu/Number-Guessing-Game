1a66f7103b979165abcc1da88aa9a4abb7264765
marcos <masadu@outlook.es>
2023-05-04 10:48:19 +0200

    holi

--- app.js
+++ app.js
@@ -2,8 +2,30 @@
 
 let estoyJugando = true;
 let numeroCorrecto = Math.floor(Math.random() * 100);
+let numerorandom = 
 let numIntentos = 10;
 
-document.querySelector("form").addEventListener("click", (e) => {
+// Actualizar el innerHTML del nodo adecuado con el número de intentos iniciales
+
+document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault(); // Impedimos que el formulario haga un petición GET
+document.getElementById("#guessfield").addEventListener("change").value
+document.querySelector
+  // Comprobar si el número que ha puesto el usuario es mayor o menor que el numeroCorrecto. Tomar decisiones
+
+  
+   
+  if( numerorandom > numeroCorrecto){ 
+console.log("TU NUMERO ES PEQUEÑO")}
+
+  else if ( numerorandom < numeroCorrecto){
+
+  console.log("Tu numero es grande")}
+
+else {
+
+console.log("HAS ACERTADO")}
+
+ 
 });
+
