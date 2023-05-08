e77f8431485797ded9129b39612bf49cd8451f3d
marcossadu <127730147+marcossadu@users.noreply.github.com>
2023-05-08 12:18:14 +0000

    esa es

--- app.js
+++ app.js
@@ -1,31 +1,131 @@
-// Good luck!
+//Generate a random number between 1 and 500
+let randomNumber = parseInt((Math.random()*100)+1);
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const startOver = document.querySelector('.resultParas');
+const lowOrHi = document.querySelector('.lowOrHi');
+const p = document.createElement('p');
+let previousGuesses = [];
+let numGuesses = 9;
+let playGame = true;
+let remainingSeconds = 10;
 
-let estoyJugando = true;
-let numeroCorrecto = Math.floor(Math.random() * 100);
-let numerorandom = 
-let numIntentos = 10;
+document.querySelector('#remaining-time').innerHTML = remainingSeconds;
 
-// Actualizar el innerHTML del nodo adecuado con el número de intentos iniciales
+// Cada segundo (1000ms) quiero que ejecutes la función updateRemainingTime
+let timer = setInterval(updateRemainingTime, 1000); 
 
-document.querySelector("form").addEventListener("submit", (e) => {
-  e.preventDefault(); // Impedimos que el formulario haga un petición GET
-document.getElementById("#guessfield").addEventListener("change").value
-document.querySelector
-  // Comprobar si el número que ha puesto el usuario es mayor o menor que el numeroCorrecto. Tomar decisiones
+function updateRemainingTime() {
+    console.log('Me ejecuto cada segundo');
+    // 1. Decremntar la variable de estado remainingSeconds
+   remainingSeconds= remainingSeconds - 1
+    // 2. Actualizar el innerHTML de #remaining-time
+    
+      document.querySelector('#remaining-time').innerHTML = remainingSeconds;
 
-  
+    // 3. Cuando llegue a 0 ha perdido
+   if(remainingSeconds == 0)
    
-  if( numerorandom > numeroCorrecto){ 
-console.log("TU NUMERO ES PEQUEÑO")}
+   {
+    clearInterval(timer)
+    endGame();
+   }
+    //   3.1 Bloquear el input para que no pueda escribir más
+    
+    // 3.2 Mostrar un mensaje que el tiempo ha finalizado e informar al usuario del numero secreto, cual era
+    // 3.3 Bonus: IMPEDIR que el usuario pueda hacer click en el botón
+    
+}
 
-  else if ( numerorandom < numeroCorrecto){
 
-  console.log("Tu numero es grande")}
+if (playGame){
+    subt.addEventListener('click', function(e){
+        e.preventDefault();
+        //Grab guess from user
+        const guess = parseInt(userInput.value);
+        validateGuess(guess);
+    });
+}
 
-else {
+function validateGuess(guess){
+    if (isNaN(guess)){
+        alert('Please enter a valid number');
+    } else if (guess < 1) {
+        alert('Please enter a number greater than 1!');
+    } else if (guess > 100){
+        alert('Please enter a number less than 500!')
+    } else {
+        //Keep record of number of attempted guesses
+        previousGuesses.push(guess);
+        //Check to see if game is over
+        if (numGuesses === 11){
+            displayGuesses(guess);
+            displayMessage(`Game Over! Number was ${randomNumber}`);
+            endGame();
+        } else {
+        //Display previous guessed numbers
+        displayGuesses(guess);
+        //Check guess and display if wrong
+        checkGuess(guess);
+        }
+    }
+}
 
-console.log("HAS ACERTADO")}
+function checkGuess(guess){
+    //Display clue if guess is too high or too low
+    if (guess === randomNumber){
+        displayMessage(`You guessed correctly!`);
+        endGame();
+    } else if (guess < randomNumber) {
+        displayMessage(`Too low! Try again!`);
+    } else if (guess > randomNumber) {
+        displayMessage(`Too High! Try again!`);
+    }
+}
 
- 
-});
+function displayGuesses(guess){
+    userInput.value = '';
+    guessSlot.innerHTML += `${guess}  `;
+    numGuesses++
+    remaining.innerHTML = `${11 - numGuesses}  `;
+}
 
+function displayMessage(message){
+        lowOrHi.innerHTML = `<h1>${message}</h1>`
+}
+
+function endGame(){
+    
+    userInput.value = '';
+   
+    userInput.setAttribute('disabled', '');
+    
+          p.classList.add('button');
+          p.innerHTML = `<h1 id="newGame">Start New Game</h1>`
+    startOver.appendChild(p);
+    playGame = false;
+    newGame();
+}
+
+function newGame(){
+    const newGameButton = document.querySelector('#newGame');
+    newGameButton.addEventListener('click', function(){
+        //Pick a new random number
+        randomNumber = parseInt((Math.random()*100)+1);
+        previousGuesses = [];
+        numGuesses = 1;
+        guessSlot.innerHTML = '';
+        lowOrHi.innerHTML = '';
+        remaining.innerHTML = `${11 - numGuesses}  `;
+        userInput.removeAttribute('disabled');
+        startOver.removeChild(p);
+        playGame = true;
+    })
+}
+//Allow to restart game with restart button
+//Change DIV to a form so it can accept the enter key
+
+//NOTES:
+//NaN != NaN
